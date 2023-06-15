6494aaaa597f4f0fa9b2424212b041119ae93766
Bhaskar Purohit <bhaskarpurohit22@gmail.com>
2023-06-15 17:19:07 +0530

    push

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import './app.css'
+import Navbar from './Components/Navbar/Navbar'
+import Home from './Components/Home/Home'
+import Footer from './Components/Footer/Footer'
+import Main from './Components/Main/Main'
 
 const App = () => {
   return (
     <div>
-        <h1>Working</h1>
-        <p>n</p>
+        <Navbar/>
+        {/* <Home/>
+        <Main/>
+        <Footer/> */}
     </div>
     
 
